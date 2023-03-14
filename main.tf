93e35f280b591a10842b88893aec22a545bb953a
kolbathiswetha <127875854+kolbathiswetha@users.noreply.github.com>
2023-03-14 21:33:57 +0530

    Add files via upload
    uploading terraform files into Capgemini repo

---
+++ main.tf
@@ -0,0 +1,31 @@
+module "my_vpc" {
+    #count = terraform.workspace == "dev" ? 1 : 0
+    source = "./modules/vpc"
+    #count  = var.create_vpc ? 1 : 0
+    vpc_cidrs = var.vpc_cidrs
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.public_subnet_cidrs
+    azs = var.azs
+}
+module "my_ec2" {
+    #count = terraform.workspace == "dev" ? 1 : 0
+    source = "./modules/ec2"
+    #count  = var.create_ec2 ? 1 : 0
+    subnet_id = module.my_vpc.subnet_id
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    depends_on = [
+      module.my_vpc
+    ]
+}
+
+module "my_alb" {
+    #count = terraform.workspace == "qa" ? 1 : 0
+    source = "./modules/alb"
+    count  = var.create_alb ? 1 : 0
+    # depends_on = [
+    #   module.my_vpc,
+    #   module.my_ec2
+    # ]
+
+}
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+    value = module.my_vpc.vpc_id
+}
+output "subnet_id" {
+    value = module.my_vpc.subnet_id  
+}
+output "instance_id" {
+    value = module.my_ec2.instance_id 
+}
+output "public_subnet_1" {
+    value = module.my_vpc.public_subnet_1 
+}
+output "public_subnet_2" {
+    value = module.my_vpc.public_subnet_2 
+}
